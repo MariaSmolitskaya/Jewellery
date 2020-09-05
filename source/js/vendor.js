0838086eb5c295869d04a402dc0800da1beb6319
Maria <smolitskaya.maria@gmail.com>
2020-09-05 15:24:04 +0300

    Исправлено мобильное меню

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -4526,3 +4526,68 @@
   }
   return svg4everybody;
 });
+
+/*
+	Main code
+*/
+
+$(document).ready(function() {
+  $('.main-header__login').modaal();
+  $('.product__add').modaal({
+    content_source: '#modal-basket'
+  });
+  $('.main__filters-btn').modaal({
+    content_source: '.filters',
+    hide_close: true
+  });
+  $('.slider__wrapper').on('init', function(event, slick) {
+    $(this).append('<div class="slick-dots-mobile"><p><span id="current">1</span> of <span id="total">'+ slick.slideCount +'</span></p></div>');
+  });
+  $('.slider__wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '.slider__btn--left',
+    nextArrow: '.slider__btn--right',
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+      }
+    ]
+  });
+  $('.slider__wrapper').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.slick-dots-mobile #current').html(currentSlide + 1);
+  });
+  $('.questions').accordion({
+    heightStyle: 'content',
+    header: '> .questions__item > h3',
+    collapsible: true
+  });
+  $('.filters__form').accordion({
+    active: 0,
+    heightStyle: 'content',
+    header: '> .filters__box > h2',
+    collapsible: true
+  });
+  $('.main-header__button-burger').click(function () {
+    $('.main-header').toggleClass('main-header--open');
+    $('body').toggleClass('overflow-hidden');
+  });
+});
+
