4b3edc6408020bdd76d92eb9083faead5c191cb7
Maria <smolitskaya.maria@gmail.com>
2020-09-16 12:57:40 +0300

    Исправления

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
   var formLogin = document.querySelector('.modal-login__button');
   var inputEmail = document.querySelector('.modal-login__input');
   inputEmail.value = localStorage.getItem('email') || '';
-  formLogin.addEventListener('click', function (e) {
+  formLogin.addEventListener('click', function () {
     localStorage.setItem('email', inputEmail.value);
   });
-})
+});
